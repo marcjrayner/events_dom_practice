f01b2cbafb842b5f1ee0994eed8728b60d6ff753
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-21 16:09:59 +0000

    refactor cretes books as divs

--- js/app.js
+++ js/app.js
@@ -8,17 +8,43 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+// const handleSubmit = function (event) {
+//   event.preventDefault()
+//
+//   const newBook = document.createElement('li')
+//   newBook.textContent = `Title: ${event.target.title.value}, Author: ${event.target.author.value}, Category: ${event.target.category.value}`
+//
+//   const bookList = document.querySelector('#reading-list')
+//   bookList.appendChild(newBook);
+//
+//   document.querySelector('#new-item-form').reset();
+// };
+
+
 const handleSubmit = function (event) {
   event.preventDefault()
 
-  const newBook = document.createElement('li')
-  newBook.textContent = `Title: ${event.target.title.value}, Author: ${event.target.author.value}, Category: ${event.target.category.value}`
+  const newTitle = document.createElement('h2')
+  newTitle.textContent = `${event.target.title.value}`
+  const newAuthor = document.createElement('h3')
+  newAuthor.textContent = `${event.target.author.value}`
+  const newCategory = document.createElement('p')
+  newCategory.textContent = `${event.target.category.value}`
+
+
+  const newBook = document.createElement('div')
+  newBook.classList.add('booklist')
+  newBook.appendChild(newTitle)
+  newBook.appendChild(newAuthor)
+  newBook.appendChild(newCategory)
+
 
   const bookList = document.querySelector('#reading-list')
   bookList.appendChild(newBook);
 
   document.querySelector('#new-item-form').reset();
 };
+
 
 const handleClear = function (event) {
 
