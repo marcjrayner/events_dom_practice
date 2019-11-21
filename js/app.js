1e8e02a46c26865363cc6fc59228204b6edbf070
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2019-11-21 15:45:57 +0000

    MVP & first extension complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+
+  const formSubmit = document.querySelector('#new-item-form');
+  formSubmit.addEventListener('submit', handleSubmit);
+
+  const clearList = document.querySelector('#clear');
+  clearList.addEventListener('click', handleClear)
+
+});
+
+const handleSubmit = function (event) {
+  event.preventDefault()
+
+  const newBook = document.createElement('li')
+  newBook.textContent = `Title: ${event.target.title.value}, Author: ${event.target.author.value}, Category: ${event.target.category.value}`
+
+  const bookList = document.querySelector('#reading-list')
+  bookList.appendChild(newBook);
+
+  document.querySelector('#new-item-form').reset();
+};
+
+const handleClear = function (event) {
+
+  let bookList = document.querySelector('#reading-list')
+  while(bookList.firstChild) {
+    bookList.removeChild(bookList.firstChild);
+  };
+
+};
